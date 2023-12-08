ae8a17aec61b803333aa40baf0c26547aa051924
jbastruz <7dfn9kr3@duck.com>
2023-12-08 16:25:50 +0100

    Update to work with UTF-8 message

--- Test.py
+++ Test.py
@@ -2,14 +2,14 @@ import requests
 import smtplib
 
 # Paramètres de l'API OpenWeatherMap
-API_KEY = "YOUR_API_KEY"
+API_KEY = "ffc1be89ebd96754d96ded5a9351de80"
 
 # Définition des fonctions
 def get_weather(city):
     """Récupère les données météo pour une ville donnée"""
 
     # URL de l'API OpenWeatherMap
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city, API_KEY)
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=fr&appid={}".format(city, API_KEY)
 
     # Récupération des données météo
     response = requests.get(url)
@@ -26,11 +26,11 @@ def send_mail(to_address, subject, body):
     # Connexion au serveur SMTP
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login("YOUR_EMAIL_ADDRESS", "YOUR_PASSWORD")
+    server.login("jbastruz@gmail.com", "xoqk asvz beou rrmh")
 
     # Envoi du mail
     message = "Subject: {}\n\n{}".format(subject, body)
-    server.sendmail("YOUR_EMAIL_ADDRESS", to_address, message)
+    server.sendmail("jbastruz@gmail.com", to_address, message.encode("utf-8"))
 
     server.quit()
 
@@ -41,10 +41,10 @@ if __name__ == "__main__":
 
     # Récupération des données météo
     weather = get_weather(city)
-
+    
     # Données météo
     temperature = weather["main"]["temp"]
     weather_description = weather["weather"][0]["description"]
 
     # Envoi du bulletin météo par mail
-    send_mail("YOUR_EMAIL_ADDRESS", "Météo à Bruxelles", "La température à Bruxelles est de {} degrés. Il fait {}.".format(temperature, weather_description))
+    send_mail("jbastruz@protonmail.com", "Météo à Bruxelles", "La température à Bruxelles est de {} degrés. Il fait {}.".format(temperature, weather_description))
